e2a5e1d7359260ab3ed6bbc7c4910cc085a57f94
Dia8897 <khalildia831@gmail.com>
2025-11-12 16:33:01 +0200

    Stop tracking backend/node_modules

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mysql from "mysql2";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+db.connect(err => {
+  if (err) console.log("DB connection error:", err);
+  else console.log("Connected to MySQL");
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
